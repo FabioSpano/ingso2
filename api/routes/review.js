3d56e95a0c84ee6493361089350e1ebeccd64434
Damiano Duranti <damianoduranti@outlook.it>
2019-12-06 10:57:35 +0100

    Minor update

--- api/routes/review.js
+++ api/routes/review.js
@@ -6,61 +6,80 @@ reviewRoutes.use(bodyParser.urlencoded({ extended: true }));
 
 const Review = require('../models/review');
 
-reviewRoutes.route('/view')
+reviewRoutes.route('/')
     .get(async function(req, res) {
-        let reviews = await Review.find({})
-        if(reviews != null){
-            res.status(200);
-            res.json([reviews]);
-            console.log('* MESSAGE: List of reviews found!');
-        }else{
-            res.status(404);
-            res.json({message: 'No reviews found!'});
-            console.log('* MESSAGE: No reviews found!');
+        try{
+            let reviews = await Review.find({})
+            if(reviews != null){
+                res.status(200);
+                res.json([reviews], {message: 'Reviews found!'});
+            }else{
+                res.status(404);
+                res.json({message: 'ERROR 404: No reviews found!'});
+            }
         }
+        catch{
+            res.status(500);
+            res.json({message: 'ERROR 500: Local server error!'});
+        }
+
     });
 
-reviewRoutes.route('/insert/:review')
+reviewRoutes.route('/')
     .post(async function(req,res){
-        var review = new Review();
-        review.review = req.params.review;
-        saved = await review.save();
-        if(saved != null){
-            res.status(201);
-            res.json([{message: 'Review correctly created'}]);
-            console.log('* MESSAGE: Review correctly created!');
-        }else{
-            res.status(404);
-            res.json({message: 'ERROR 404: Review not created!'});
-            console.log('* MESSAGE: Review not created!');
+        try{
+            var review = new Review();
+            review.review = req.query.review;
+            saved = await review.save();
+            if(saved != null){
+                res.status(201);
+                res.json([{message: 'Review correctly created'}]);
+            }else{
+                res.status(400);
+                res.json({message: 'ERROR 400: Review not created, probably no review written!});
+            }
+        }
+        catch{
+            res.status(500);
+            res.json({message: 'ERROR 500: Local server error!'});
         }
     });
 
 reviewRoutes.route('/:review_id')
     .get(function(req,res){
-        let review_id = Review.findMyReview(req.params.review_id);
-        if(review_id != null){
-            res.status(200);
-            res.json({review_id, message: 'Review correctly found!'});
-            console.log('* MESSAGE: Review correctly found!');
-        }else{
-            res.status(404);
-            res.json({message: 'Review not found'});
-            console.log('* MESSAGE: Review not found!');
+        try{
+            let review_id = Review.findMyReview(req.params.review_id);
+            if(review_id != null){
+                res.status(200);
+                res.json({review_id, message: 'Review correctly found!'});
+            }else{
+                res.status(404);
+                res.json({message: 'ERROR 404: Review not found'});
+            }
         }
+        catch{
+            res.status(500);
+            res.json({message: 'ERROR 500: Local server error!'});
+        }
+
     });
 
-reviewRoutes.route('/delete')
+reviewRoutes.route('/')
     .delete(function (req, res) {
-        if(Review.delete()){
-            res.status(200);
-            res.json({message: 'All reviews are cancelled'});
-            console.log('* MESSAGE: All reviews are cancelled!');
-        }else{
-            res.status(404);
-            res.json({message: 'ERROR 404: All reviews are already cancelled'});
-            console.log('* MESSAGE: All reviews are already cancelled!');
+        try{
+            if(Review.delete()){
+                res.status(200);
+                res.json({message: 'All reviews are cancelled'});
+            }else{
+                res.status(404);
+                res.json({message: 'ERROR 404: All reviews are already cancelled'});
+            }
         }
+        catch{
+            res.status(500);
+            res.json({message: 'ERROR 500: Local server error!'});
+        }
+
     });
 
 module.exports = reviewRoutes;
